97e39e8f8586fe67e79df5ee79e282ee55537146
Sebastian Brosch <code@sebastianbrosch.de>
2025-03-22 00:30:48 +0100

    fixed profile startup

--- Profile.ps1
+++ Profile.ps1
@@ -1,6 +1,6 @@
 $ProfileFolder = Split-Path $PROFILE
-Import-Module $(Join-Path $ProfileFolder "Modules" "PowerShell")
-Import-Module $(Join-Path $ProfileFolder "Modules" "Utils")
+Import-Module $(Join-Path -Path $ProfileFolder -ChildPath "Modules\PowerShell")
+Import-Module $(Join-Path -Path $ProfileFolder -ChildPath "Modules\Utils")
 
 Write-Host -ForegroundColor Green "You are using the PowerShell profile $(Get-PP_ProfileName $PROFILE)."
 Write-Host -ForegroundColor Green "Your profile is located at: $ProfileFolder"
